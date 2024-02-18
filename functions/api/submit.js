0ebfea314fdeea9ef85868691f1013b139872409
socialitin <lmacias@tournet.com>
2024-02-17 21:05:48 -0500

    ok

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -27,6 +27,27 @@ export async function onRequestPost({ request }) {
 		return new Response('Error parsing JSON content', { status: 400 });
 	}
 }
+addEventListener('fetch', event => {
+	event.respondWith(handleRequest(event.request))
+	})
+	
+	async function handleRequest(request) {
+	const url = new URL(request.url)
+	const jsonUrl = 'https://pub-ff67a151dd104cf6b171f45a47c36526.r2.dev/NYCD.json' // Replace this with the URL of the R2 JSON file
+	
+	if (url.pathname === '/load-r2-json') {
+	const response = await fetch(jsonUrl)
+	const json = await response.json()
+	
+	return new Response(JSON.stringify(json), {
+	headers: {
+	'Content-Type': 'application/json'
+	}
+	})
+	} else {
+	return new Response('Not Found', { status: 404 })
+	}
+	}
 export default {
 	async fetch(request, env) {
 	const url = new URL(request.url);
