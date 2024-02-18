8e6f94e84fa7fd8a7d45c33f9e8210c776c425d1
socialitin <lmacias@tournet.com>
2024-02-17 20:44:48 -0500

    get json r2

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -27,3 +27,25 @@ export async function onRequestPost({ request }) {
 		return new Response('Error parsing JSON content', { status: 400 });
 	}
 }
+export default {
+	async fetch(request, env) {
+	const url = new URL(request.url);
+	const key = url.pathname.slice(1);
+	
+	if (request.method === 'GET') {
+	const r2Object = await env.MY_BUCKET.get(key);
+	if (r2Object) {
+	const json = await r2Object.json();
+	return new Response(JSON.stringify(json), {
+	headers: {
+	'Content-Type': 'application/json',
+	},
+	});
+	} else {
+	return new Response('File not found', { status: 404 });
+	}
+	} else {
+	return new Response('Method not allowed', { status: 405 });
+	}
+	},
+	};
