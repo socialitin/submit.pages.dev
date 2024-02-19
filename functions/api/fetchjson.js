88bbef1291e0c784506b3d45c72b645f0f2ec7e0
socialitin <lmacias@tournet.com>
2024-02-19 16:29:13 -0500

    j
    k

--- functions/api/fetchjson.js
+++ functions/api/fetchjson.js
@@ -5,15 +5,15 @@
 //	return context.env.SERVICE.fetch(context.request);
   //}
 
-export async function onRequestPost({ request }) {
+export async function onRequestGet({ request }) {
 	try {
         const url = 'https://pub-ff67a151dd104cf6b171f45a47c36526.r2.dev/NYCD.json';
-        fetch('url');
+       const myj = fetch('url');
      // .then(response => response.json())
      // .then(data => console.log(data))
     // .catch(error => console.error('Error:', error));
 
-let pretty = JSON.stringify(response, null, 2);
+let pretty = JSON.stringify(myj, null, 2);
 		return new Response(pretty, {
 			headers: {
 				'Content-Type': 'application/json;charset=utf-8'
