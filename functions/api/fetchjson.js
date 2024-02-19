aedadf5ca41213c92477218b475cc47256155c0c
socialitin <lmacias@tournet.com>
2024-02-19 16:24:04 -0500

    Update fetchjson.js

--- functions/api/fetchjson.js
+++ functions/api/fetchjson.js
@@ -8,12 +8,12 @@
 export async function onRequestPost({ request }) {
 	try {
         const url = 'https://pub-ff67a151dd104cf6b171f45a47c36526.r2.dev/NYCD.json';
-        fetch('url')
-      .then(response => response.json())
-      .then(data => console.log(data))
-      .catch(error => console.error('Error:', error));
+        fetch('url');
+     // .then(response => response.json())
+     // .then(data => console.log(data))
+    // .catch(error => console.error('Error:', error));
 
-let pretty = JSON.stringify(data, null, 2);
+let pretty = JSON.stringify(response, null, 2);
 		return new Response(pretty, {
 			headers: {
 				'Content-Type': 'application/json;charset=utf-8'
