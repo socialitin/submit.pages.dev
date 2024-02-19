4f96a84e5d74d1c494874415d7e93751de61c117
socialitin <lmacias@tournet.com>
2024-02-19 16:11:53 -0500

    Update fetchjson.js

--- functions/api/fetchjson.js
+++ functions/api/fetchjson.js
@@ -1,6 +1,11 @@
 /**
  * POST /api/submit
  */
-export async function onRequestGet(fetchjson) {
-	return fetchjson.env.SERVICE.fetch(fetchjson.request);
+export async function onRequestGet(context) {
+	return context.env.SERVICE.fetch(context.request);
   }
+  const url = 'https://pub-ff67a151dd104cf6b171f45a47c36526.r2.dev/NYCD.json';
+  fetch('url')
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(error => console.error('Error:', error));
