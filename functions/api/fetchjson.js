d0d5df4b5633b927fda4442f6f6518db4cafaab7
socialitin <lmacias@tournet.com>
2024-02-19 16:54:49 -0500

    Update fetchjson.js

--- functions/api/fetchjson.js
+++ functions/api/fetchjson.js
@@ -1,20 +1,14 @@
 /**
  * POST /api/submit
  */
-//export async function onRequestGet(context) {
-//	return context.env.SERVICE.fetch(context.request);
-  //}
+
 
 export async function onRequestGet({ request }) {
-	try {
+	
         async function handleRequest(request) {
             // Make a request to the R2 JSON file
             const response = await fetch('https://pub-ff67a151dd104cf6b171f45a47c36526.r2.dev/NYCD.json')
           
-            // Check if the request was successful
-            if (!response.ok) {
-              return new Response('Failed to fetch R2 JSON file', { status: 500 })
-            }
           
             // Parse the JSON response
             const data = await response.json()
@@ -25,6 +19,6 @@ export async function onRequestGet({ request }) {
               headers: { 'Content-Type': 'application/json' }
             })
           }		
-	} 
-};
+	 
+}
 
