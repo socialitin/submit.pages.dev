ac3bc082eba8aac177e2871234f4c7f5dc10bbe5
socialitin <lmacias@tournet.com>
2024-02-20 12:44:45 -0500

    Update fetchjson.js

--- functions/api/fetchjson.js
+++ functions/api/fetchjson.js
@@ -1,9 +1,9 @@
 export async function onRequest(context) {
-  const obj = await context.env.BUCKET.get('_BZ0wdHFj8CHeDPMEY28XbGjemKlggsDyQcZUSG5 ');
+  const obj = await context.env.BUCKET.get('_BZ0wdHFj8CHeDPMEY28XbGjemKlggsDyQcZUSG5');
   if (obj === null) {
     return new Response('Not found', { status: 404 });
   }
   return new Response(obj.body);
 }
-//}
+
 
