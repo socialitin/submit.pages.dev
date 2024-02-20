0108913a00117d409e33b527531f6659073071a8
socialitin <lmacias@tournet.com>
2024-02-20 12:39:02 -0500

    r2

--- functions/api/fetchjson.js
+++ functions/api/fetchjson.js
@@ -1,24 +1,9 @@
-/**
- * POST /api/submit
- */
-
-
-////export async function onRequestGet({ request }) {
-	
-        async function handleRequest(request) {
-            // Make a request to the R2 JSON file
-            const response = await fetch('https://pub-ff67a151dd104cf6b171f45a47c36526.r2.dev/NYCD.json')
-          
-          
-            // Parse the JSON response
-            const data = await response.json()
-          
-            // Do something with the JSON data
-            // For example, you can return it as a response
-            return new Response(JSON.stringify(data), {
-              headers: { 'Content-Type': 'application/json' }
-            })
-          }		
-	 
+export async function onRequest(context) {
+  const obj = await context.env.BUCKET.get('_BZ0wdHFj8CHeDPMEY28XbGjemKlggsDyQcZUSG5 ');
+  if (obj === null) {
+    return new Response('Not found', { status: 404 });
+  }
+  return new Response(obj.body);
+}
 //}
 
