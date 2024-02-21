253ce6a7b9d9dae1a553e0432546c0f6a1140c99
socialitin <lmacias@tournet.com>
2024-02-21 17:13:10 -0500

    Update fetchjson.js

--- functions/api/fetchjson.js
+++ functions/api/fetchjson.js
@@ -4,25 +4,67 @@ export async function onRequest(context) {
     return new Response('Not found', { status: 404 });
   }
 
-  const offers = [];
+  var offers = [];
 Object.keys(obj).forEach(function(key,elem) {
-  //const tRgn = 0;
-  const value = obj[key];
-  const str = elem.toString();
+  var value = obj[key];
+  var str = elem.toString();
 //$('#'+str+'').name = key.substring(0,2)+str;
-const tRgn = 0;
-//const value = pAnnts[key];
-//const str = elem.toString();
+
   //$('#'+str+'').name = key.substring(0,2)+str;
 //$('#'+str+'').show();
+var tValue = '';
+var tpics = []; 
+var pname = '';
+var cRegion = key;
+var aRegion = key.substring(0,3);
+//+elem;
 
-//tValue = ' ';
-//tpics = []; 
-//pname = '';
-const cRegion = key;
-const aRegion = key.substring(0,3);
-offers.push(value)
+rDetail = key.substring(0,2)+str+',';
+ // console.warn('current', key,value[0].Province,value[0].Pitch);
+var rVideo = value[0].Pitch; 
+if (rVideo.includes("src=")) { 
+  //use it   
+ }else{
+   var rVideo = ` <div style="position: relative; padding-top: 56.25%;"><iframe src="`+rVideo+ `" style="border: none; position: absolute; top: 0; left: 0; height: 100%; width: 100%;" allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;" allowfullscreen="true"></iframe></div>`;  
+ };
+
+for (var h = 1; h <value.length; h++ ) {
+    var rDetail = rDetail+value[h].IATA+',';
+
+var tValue = tValue + value[h].City + ' * ' ;
+
+   // if ($('#tournet-nearby').attr('options') == 0){
+//document.getElementById(''+str+'').src = tImg; 
+//document.getElementById('xy'+str+'').innerHTML = mLabel;
+   //  };
+//var pcountry = $('#myItin').attr('data-country')+ '/';       
+var pcountry = "US_";  
+//var tImg = "https://mmedia.tournet.com/Tournet/Destinations/Presentation/"+ pcountry +cRegion+'/'+ value[h].City+'.jpg';
+//var Tpics = {"img": tImg , "name" : value[h].City, "description" : value[h].Pitch, "iata" : value[h].IATA};
+//tpics.push(Tpics);
+      
+var thisoffer = {
+  Ofr_OfferId: 555,
+ Ofr_Name:  tValue,
+ Ofr_Location: cRegion,
+ Cmp_CompanyId: "431", 
+Ofr_Pictures: JSON.stringify(tpics),
+ Ofr_Type: "C",
+ Ofr_Instructions: rDetail,
+ Ofr_AutoResponse: 'NY',
+ Pds_ProdServiceId: "272744",
+ Ofr_WgtLanguages: "en" ,
+   Ofr_Published: "204",
+ Pdt_Type: "P",
+   Ofr_EmVideo: ''
+ };
+
+};
+//if ($('#tournet-nearby').attr('options') == 0){
+  //document.getElementById(''+str+'').name = rDtl.substring(4);
+// };
+offers.push(thisoffer);
 });
-  //return new Response(obj.body);
-  return new Response(offers);
+  return new Response(obj.body);
+ // return new Response(offers);
 }
