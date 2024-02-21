979b6134343aed18f3ed69d1f70d6649473bfcbd
socialitin <lmacias@tournet.com>
2024-02-21 13:10:12 -0500

    Update fetchjson.js

--- functions/api/fetchjson.js
+++ functions/api/fetchjson.js
@@ -4,21 +4,6 @@ export async function onRequest(context) {
     return new Response('Not found', { status: 404 });
   }
       
-var thisoffer = {
-  Ofr_OfferId: 555,
- Ofr_Name:  tValue,
- Ofr_Location: cRegion,
- Cmp_CompanyId: "431", 
-Ofr_Pictures: JSON.stringify(tpics),
- Ofr_Type: "C",
- Ofr_Instructions: rDetail,
- Ofr_AutoResponse: 'NY',
- Pds_ProdServiceId: "272744",
- Ofr_WgtLanguages: "en" ,
-   Ofr_Published: "204",
- Pdt_Type: "P",
-   Ofr_EmVideo: ''
- };
  
   return new Response(obj.body);
   //return new Response(offers);
