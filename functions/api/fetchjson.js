0fab58dd9f28c640cc927287228f166acdd8e2bb
socialitin <lmacias@tournet.com>
2024-02-21 12:57:25 -0500

    Update fetchjson.js

--- functions/api/fetchjson.js
+++ functions/api/fetchjson.js
@@ -1,5 +1,9 @@
 export async function onRequest(context) {
   const obj = await context.env.filterjson.get('430.json');
+  if (obj === null) {
+    return new Response('Not found', { status: 404 });
+  }
+  
 pAnnts = obj;
   var offers = [];
 Object.keys(pAnnts).forEach(function(key,elem) {
@@ -75,9 +79,7 @@ offers.push(thisoffer);
 
 });
 
-  if (obj === null) {
-    return new Response('Not found', { status: 404 });
-  }
+ 
   return new Response(obj.body);
   //return new Response(offers);
 }
