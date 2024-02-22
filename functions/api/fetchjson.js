db10c9b6a293bee581879ba4495e8d9ea87e67ac
socialitin <lmacias@tournet.com>
2024-02-22 16:33:08 -0500

    Update fetchjson.js

--- functions/api/fetchjson.js
+++ functions/api/fetchjson.js
@@ -4,7 +4,7 @@ export async function onRequest(context) {
     return new Response('Not found', { status: 404 });
   }
   
-    return new Response(obj.body);
+    return new Response(obj.body[City]);
    // return new Response(offers);
 
 };
