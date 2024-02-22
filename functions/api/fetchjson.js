f1969554292ba1e26867b65307f64ae755a8f9f3
socialitin <lmacias@tournet.com>
2024-02-22 16:14:15 -0500

    Update fetchjson.js

--- functions/api/fetchjson.js
+++ functions/api/fetchjson.js
@@ -4,26 +4,26 @@ export async function onRequest(context) {
     return new Response('Not found', { status: 404 });
   }
 
-    var rDetail = obj[0].IATA;
-    var tValue = obj[0].City;
-    var cRegion = obj[0].Province;
-  var tpics = [];
-  var rVideo = obj[0].Pitch;
+    //var rDetail = obj[0].IATA;
+    //var tValue = obj[0].City;
+    //var cRegion = obj[0].Province;
+  //var tpics = [];
+  //var rVideo = obj[0].Pitch;
   
     var thisoffer = {
       Ofr_OfferId: 555,
-     Ofr_Name:  tValue,
-     Ofr_Location: cRegion,
+     Ofr_Name:  obj[0].City,
+     Ofr_Location: obj[0].Province,
      Cmp_CompanyId: "431", 
-  Ofr_Pictures: JSON.stringify(tpics),
+  Ofr_Pictures: [],
      Ofr_Type: "C",
-     Ofr_Instructions: rDetail,
+     Ofr_Instructions: obj[0].IATA,
      Ofr_AutoResponse: 'NY',
      Pds_ProdServiceId: "272744",
      Ofr_WgtLanguages: "en" ,
        Ofr_Published: "204",
      Pdt_Type: "P",
-       Ofr_EmVideo: rVideo
+       Ofr_EmVideo: obj[0].Pitch
      };
     
     return new Response(obj.body);
