e2ee12e9578ab22c8e135aef137b531ac38269ea
socialitin <lmacias@tournet.com>
2024-02-24 14:44:03 -0500

    h

--- functions/api/fetchjson.js
+++ functions/api/fetchjson.js
@@ -3,8 +3,8 @@ export async function onRequest(context) {
   if (obj === null) {
     return new Response('Not found', { status: 404 });
   }
-    //return new Response(obj.body);
+    return new Response(obj.body);
    //return new Response(JSON.stringify(obj))
-   return new Response(obj.City);
+   //return new Response(obj.City);
 
 };
