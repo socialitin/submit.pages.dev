f47be8d88b80514325d4f682794b498a4ab840f4
socialitin <lmacias@tournet.com>
2024-02-26 14:14:36 -0500

    Update postjson.js

--- functions/api/postjson.js
+++ functions/api/postjson.js
@@ -1,38 +1,4 @@
-/**
- * POST /api/submit
- */
 
-export async function onRequestPost({ request }) {
-	try {
-		let input = await request.formData();
-
-		// Convert FormData to JSON
-		// NOTE: Allows mutliple values per key
-		let tmp, output = {};
-		for (let [key, value] of input) {
-			tmp = output[key];
-			if (tmp === undefined) {
-				output[key] = value;
-			} else {
-				output[key] = [].concat(tmp, value);
-			}
-		}
-
-
-let pretty = JSON.stringify(output, null, 2);
-		return new Response(pretty, {
-			headers: {
-				'Content-Type': 'application/json;charset=utf-8'
-			}
-		});
-					
-	} catch (err) {
-		return new Response('Error parsing JSON content', { status: 400 });
-	}
-}
-export async function onRequestGet(context) {
-	return context.env.SERVICE.fetch(context.request);
-  }
 ////// post modified json
 addEventListener('fetch', event => {
     event.respondWith(handleRequest(event.request))
@@ -51,7 +17,7 @@ addEventListener('fetch', event => {
     // Make an HTTP request to the r2 API endpoint with the modified JSON data
     export async function onRequest(context) {
         const obj = await context.env.filterjson.get('NYC-Video.json')
-        
+
     const response = await fetch('https://api.example.com/r2', {
       method: 'POST',
       headers: {
