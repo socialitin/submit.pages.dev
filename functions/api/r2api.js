ced574abf65ba279d8fd673fc4c031a930c305d7
socialitin <lmacias@tournet.com>
2024-02-27 12:31:28 -0500

    k

--- functions/api/r2api.js
+++ functions/api/r2api.js
@@ -6,7 +6,7 @@ export default {
     switch (request.method) {
       case 'PUT':
         await env.filterjson.put('NYC-SAN.json', request.body);
-        //return new Response(`Put ${key} successfully!`);
+        return new Response(`Put ${key} successfully!`);
       case 'GET':
         const object = await env.filterjson.get('NYC-Video.json');
 
