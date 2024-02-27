d0df44e9df067529947fb384cacce6f479058455
socialitin <lmacias@tournet.com>
2024-02-27 15:46:08 -0500

    d

---
+++ functions/api/jsond1.js
@@ -0,0 +1,15 @@
+    export async function onRequest(context) {
+        const obj = await context.env.filterjson.get('NYC-Video.json');
+        if (obj === null) {
+          return new Response('Not found', { status: 404 });
+        }
+        const modifiedData = {
+          City: 'Cali',
+          
+        }
+        const jsonData = JSON.stringify(modifiedData)
+        
+          return new Response(obj.body);
+         //return new Response(JSON.stringify(obj))
+         //return new Response(obj.City);
+      };
