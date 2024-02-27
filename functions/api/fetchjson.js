e90513e905760dab496172d77964d7dfb1dcf0c9
socialitin <lmacias@tournet.com>
2024-02-27 12:12:01 -0500

    g
    g

--- functions/api/fetchjson.js
+++ functions/api/fetchjson.js
@@ -1,37 +1,15 @@
-export default {
-  async fetch(request, env) {
-    const url = new URL('https://pub-ff67a151dd104cf6b171f45a47c36526.r2.dev/NYC-Video.json');
-    const key = url.pathname.slice(1);
-console.log(key);
-    switch (request.method) {
-      case 'PUT':
-        await env.filterjson.put(key, request.body);
-        return new Response(`Put ${key} successfully!`);
-      case 'GET':
-        const object = await env.filterjson.get(key);
-
-        if (object === null) {
-          return new Response('Object Not Found', { status: 404 });
-        }
-
-        const headers = new Headers();
-        object.writeHttpMetadata(headers);
-        headers.set('etag', object.httpEtag);
-
-        return new Response(object.body, {
-          headers,
-        });
-      case 'DELETE':
-        await env.MY_BUCKET.delete(key);
-        return new Response('Deleted!');
-
-      default:
-        return new Response('Method Not Allowed', {
-          status: 405,
-          headers: {
-            Allow: 'GET',
-          },
-        });
-    }
-  },
+export async function onRequest(context) {
+  const obj = await context.env.filterjson.get('NYC-Video.json');
+  if (obj === null) {
+    return new Response('Not found', { status: 404 });
+  }
+  const modifiedData = {
+    City: 'Cali',
+    
+  }
+  const jsonData = JSON.stringify(modifiedData)
+  
+    return new Response(obj.body);
+   //return new Response(JSON.stringify(obj))
+   //return new Response(obj.City);
 };
--- functions/api/r2api.js
+++ functions/api/r2api.js
@@ -1,20 +1,3 @@
-export async function onRequest(context) {
-  const obj = await context.env.filterjson.get('NYC-Video.json');
-  if (obj === null) {
-    return new Response('Not found', { status: 404 });
-  }
-  const modifiedData = {
-    City: 'Cali',
-    
-  }
-  const jsonData = JSON.stringify(modifiedData)
-  
-    return new Response(obj.body);
-   //return new Response(JSON.stringify(obj))
-   //return new Response(obj.City);
-
-};
-/////
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
@@ -51,5 +34,4 @@ export default {
         });
     }
   },
-};
-
+}
