99ff0b71a94eb0db0db47d9c351e329e87de49d1
socialitin <lmacias@tournet.com>
2024-02-27 12:37:50 -0500

    Update submit.js

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -33,7 +33,7 @@ let pretty = JSON.stringify(output, null, 2);
 		return new Response('Error parsing JSON content', { status: 400 });
 	}
 }
-console.log('current json', $("#pitch"));
+console.log('current json', context.env.SERVICE.fetch(context.request));
 export async function onRequestGet(context) {
 return context.env.SERVICE.fetch(context.request);
 
