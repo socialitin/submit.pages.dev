38fa6494d0fc55a0c907dfb1e5bcb910c17d3920
socialitin <lmacias@tournet.com>
2024-02-28 17:21:02 -0500

    u

--- functions/api/jsond1.js
+++
@@ -1,6 +0,0 @@
-      export default {
-        async fetch(request, env, ctx) {
-        const res = await env.DB.prepare("SELECT * FROM jcontent;").all();
-        return new Response(JSON.stringify(res, null, 2));
-        },
-        };
