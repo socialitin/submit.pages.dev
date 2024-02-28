f7639dd975a86b0f996b61c4c80ded80775e649b
socialitin <lmacias@tournet.com>
2024-02-28 13:14:01 -0500

    s

--- functions/api/tnhosts.ts
+++ functions/api/tnhosts.ts
@@ -1,4 +1,3 @@
-// src/index.ts
 var src_default = {
     async fetch(request, env) {
       const { pathname } = new URL(request.url);
@@ -6,18 +5,11 @@ var src_default = {
         const { results } = await env.DB.prepare(
           "SELECT * FROM Hosts WHERE CompanyName = ?"
         ).bind("Bs Beverages").all();
-        var Response: {
-            new (body?: BodyInit | null | undefined, init?: ResponseInit | undefined): Response;
-            prototype: Response;
-            error(): Response;
-            redirect(url: string | URL, status?: number | undefined): Response;
-        }
+        return Response.json(results);
       }
       //return new Response(
         "Call /api/beverages to see everyone who works at Bs Beverages"
-      
-     // );
-    
+      //);
     }
   };
   export {
