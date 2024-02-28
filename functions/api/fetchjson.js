965ed60abadb8dbcbac9fe782255066880a09d76
socialitin <lmacias@tournet.com>
2024-02-28 16:57:19 -0500

    u

--- functions/api/fetchjson.js
+++ functions/api/fetchjson.js
@@ -1,7 +1,6 @@
 export default {
   async fetch(request, env, ctx) {
   const res = await env.DB.prepare("SELECT * FROM jcontent;").all();
-  return new Response;
-  //(JSON.stringify(res, null, 2));
+  return new Response(JSON.stringify(res, null, 2));
   },
   };
