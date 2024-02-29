eb29b5c42740412ae7e90dd2c89ca280016afcc6
socialitin <lmacias@tournet.com>
2024-02-29 12:30:55 -0500

    Update d1tnhosts.js

--- functions/d1tnhosts.js
+++ functions/d1tnhosts.js
@@ -2,6 +2,7 @@ fetch('d1-tn-hosts.socialitin.workers.dev')
 .then(response => response.text())
 .then(data => {
 console.log(data);
+return data;
 })
 .catch(error => {
 console.error('There was an error!', error);
