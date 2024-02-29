af0f026b650e3a4e42ebc878bc39f3011a5cadab
socialitin <lmacias@tournet.com>
2024-02-29 12:27:50 -0500

    Update d1tnhosts.js

--- functions/d1tnhosts.js
+++ functions/d1tnhosts.js
@@ -1,14 +1,8 @@
-const src_default = {
-    fetch: async function(request, env) {
-      const { pathname } = new URL(request.url);
-      //if (pathname === "/api/beverages") {
-        const { results } = await env.DB.prepare(
-          "SELECT * FROM Hosts WHERE CompanyName = ?"
-        ).bind("Bs Beverages").all();
-        console.log('got',Response.json(results));
-        return Response.json(results);
-      //}
-    }
-  };
-  export default src_default;
-  //# sourceMappingURL=index.js.map
+fetch('d1-tn-hosts.socialitin.workers.dev')
+.then(response => response.text())
+.then(data => {
+console.log(data);
+})
+.catch(error => {
+console.error('There was an error!', error);
+});
