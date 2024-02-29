867721e9fd11bba6c3c901a7bbd921eb2aba03a6
socialitin <lmacias@tournet.com>
2024-02-29 15:29:34 -0500

    d

---
+++ functions/api/d1tnhosts.js
@@ -0,0 +1,14 @@
+const src_default = {
+    fetch: async function(request, env) {
+      const { pathname } = new URL(request.url);
+      //if (pathname === "/api/beverages") {
+        const { results } = await env.DB.prepare(
+          "SELECT * FROM Hosts WHERE CompanyName = ?"
+        ).bind("Bs Beverages").all();
+        return Response.json(results);
+      //}
+    }
+  };
+  export default src_default;
+  //# sourceMappingURL=index.js.map
+    
