ee660c82498303d5f65857e3434521979386467d
socialitin <lmacias@tournet.com>
2024-03-01 17:54:33 -0500

    Update d1tnhosts.js

--- functions/api/d1tnhosts.js
+++ functions/api/d1tnhosts.js
@@ -1,12 +1,6 @@
   export async function onRequest(context) {
     const ps = context.env.DB.prepare('SELECT * from hosts');
     const data = await ps.first();
-  
-    return Response.json(ps);
+      return Response.json(ps);
   }
-    //json_extract('{ps}', '$.City');
-    // Create a prepared statement with our query
-    //const ps =  context.env.DB.prepare(hosts SET pitching = json_replace(pitching, '$.City[#]', 'US2-EasternCali') WHERE City = 'US2-Eastern');
-    //const ps =  context.env.db.prepare("UPDATE hosts SET pitching = json_replace(pitching, '$.City', 'Cali') WHERE '$.City' = 'US2-Eastern'")
-    //?1 WHERE City = ?2").bind('US2-Eastern', 'US2-Eastern2'));
-    ///UPDATE hosts SET pitching = json_replace(pitching, '$.City', 'Cali') WHERE '$.City' = 'US2-Eastern'
+  
