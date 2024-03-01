c722f6b8296b18b3ac079a000ead1af9009e825f
socialitin <lmacias@tournet.com>
2024-03-01 17:34:09 -0500

    4

--- functions/api/d1tnhosts.js
+++ functions/api/d1tnhosts.js
@@ -1,11 +1,14 @@
   export async function onRequest(context) {
     // Create a prepared statement with our query
     //const ps =  context.env.DB.prepare(hosts SET pitching = json_replace(pitching, '$.City[#]', 'US2-EasternCali') WHERE City = 'US2-Eastern');
-    const ps =  context.env.db.prepare(("UPDATE hosts SET City = ?1 WHERE City = ?2").bind('US2-Eastern', 'US2-Eastern2'));
+    const ps =  context.env.db.prepare("UPDATE hosts SET pitching = json_replace(pitching, '$.City', 'Cali') WHERE '$.City' = 'US2-Eastern'")
+    //?1 WHERE City = ?2").bind('US2-Eastern', 'US2-Eastern2'));
+    ///UPDATE hosts SET pitching = json_replace(pitching, '$.City', 'Cali') WHERE '$.City' = 'US2-Eastern'
     //const ps = context.env.DB.prepare('SELECT pitching from hosts');
     //json_extract('{ps}', '$.City');
     //const data = await ps.first();
   
-    return Response.json(ps);
+    return Response(ps)
+    //json(ps);
   }
  
