dcaa54b507573327ed1d1074fd1251c63df7de8f
socialitin <lmacias@tournet.com>
2024-03-01 13:25:10 -0500

    2

--- functions/api/d1tnhosts.js
+++ functions/api/d1tnhosts.js
@@ -1,6 +1,6 @@
   export async function onRequest(context) {
     // Create a prepared statement with our query
-    const ps = context.env.DB.prepare('SELECT * from hosts');
+    const ps = context.env.DB.prepare('SELECT pitch from hosts');
     const data = await ps.first();
   
     return Response.json(data);
