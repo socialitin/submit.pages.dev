d795394876e70fa11f47497c9ee3c21f1806940e
socialitin <lmacias@tournet.com>
2024-03-02 13:07:24 -0500

    Update d1tnhosts.js

--- functions/api/d1tnhosts.js
+++ functions/api/d1tnhosts.js
@@ -1,5 +1,8 @@
   export async function onRequest(context) {
-    const ps = context.env.DB.prepare('SELECT * from hosts');
-    const data = await ps.first();
-      return Response.json(data);
+ //   const ps = context.env.DB.prepare('SELECT * from hosts');
+ //   const data = await ps.first();
+ //     return Response.json(data);
+ const info = await env.DB.prepare('INSERT INTO hosts pitching VALUES {"City": "Cali"}');
+const dat = await info;
+return Response.json(dat);
   }
