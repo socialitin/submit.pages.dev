ca1790297b63d76911e56e2b74666c41eb50e0c9
socialitin <lmacias@tournet.com>
2024-03-02 12:48:11 -0500

    Update submit.js

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -27,9 +27,9 @@ export async function onRequestPost({ request }) {
 let pretty = JSON.stringify(output, null, 2);
 
 console.log(pretty);
-const info = await env.DB.prepare('UPDATE hosts SET CompanyName = "Tournet"').run();
+//const info = await env.DB.prepare('UPDATE hosts SET CompanyName = "Tournet"').run();
 
-		return new Response('Updated?');
+		return new Response({'Updated': '?'});
 		
 	} catch (err) {
 		return new Response('Error parsing JSON content', { status: 400 });
