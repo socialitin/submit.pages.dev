2f8cd8ba3d2b72edbd2746d69c492a0b4df34c93
socialitin <lmacias@tournet.com>
2024-03-02 12:10:08 -0500

    v

--- functions/api/d1tnhosts.js
+++ functions/api/d1tnhosts.js
@@ -1,5 +1,5 @@
   export async function onRequest(context) {
-    const ps = context.env.DB.prepare('SELECT * from jsondata');
+    const ps = context.env.DB.prepare('SELECT * from NYC-Video');
     const data = await ps.first();
       return Response.json(data);
   }
