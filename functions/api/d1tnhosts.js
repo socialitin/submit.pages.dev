c26e200ca7ff230bcf620055cd60a7d36609217b
socialitin <lmacias@tournet.com>
2024-03-02 17:04:50 -0500

    Update d1tnhosts.js

--- functions/api/d1tnhosts.js
+++ functions/api/d1tnhosts.js
@@ -2,7 +2,7 @@
  //   const ps = context.env.DB.prepare('SELECT * from hosts');
  //   const data = await ps.first();
  //     return Response.json(data);
- const info = await env.DB.prepare('INSERT INTO jsondata pitch VALUES {"City": "Cali"}');
+ const info = await env.DB.prepare('INSERT INTO jsondata (pitch) VALUES (`{"City": "Cali"}`)');
 const dat = await info;
 return Response.json(dat);
   }
