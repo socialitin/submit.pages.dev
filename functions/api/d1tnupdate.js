b5cbfd405f418ebbbf43a7c7dbfce1def29da5ea
socialitin <lmacias@tournet.com>
2024-03-08 17:30:33 -0500

    Update d1tnupdate.js

--- functions/api/d1tnupdate.js
+++ functions/api/d1tnupdate.js
@@ -1,7 +1,7 @@
-    const stmt = context.env.DB.prepare("UPDATE hosts SET CompanyName = 'Pereira' WHERE CompanyName = 'tn' ");
-
-   // const data = await stmt;
-     //     return Response.json(data);
+export async function onRequest(context) {
+   const stmt = context.env.DB.prepare("UPDATE hosts SET CompanyName = 'Pereira' WHERE CompanyName = 'tn' ");
+   const data = await StrictMode;
+    return Response.json(data);}
 
    // export async function onRequest(context) {
     //    const ps = context.env.DB.prepare('SELECT * from hosts');
