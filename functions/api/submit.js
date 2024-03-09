47a0dff978780865daf07779ce78d8d279c6c88f
socialitin <lmacias@tournet.com>
2024-03-09 14:44:47 -0500

    Update submit.js

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -3,7 +3,7 @@
  */
 
 export async function onRequest(context) {
-    const stmt = context.env.DB.prepare("UPDATE hosts SET CompanyName = 'Pereira' WHERE CompanyName LIKE '%tn%' ");
+    const stmt = context.env.DB.prepare("UPDATE hosts SET CompanyName = 'Cartagena' WHERE CompanyName LIKE '%tn%' ");
     const result = await stmt.run(); // Execute the prepared statement
 
     // Check the result of the update operation
