f61f089383cfb071d047c7902f31f2c702d2b607
socialitin <lmacias@tournet.com>
2024-03-09 13:55:43 -0500

    Update submit.js

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -23,7 +23,7 @@ let pretty = JSON.stringify(output, null, 2);
 var toupd = $('<img id="fjson" src="https://tournet.com/SocialITIN/img/feedicon.jpg" width="32px" height="32px" style="margin-top:10px; float:right;" onclick="d1tnupdate()">');
 console.log(pretty);
 
-		return new Response(SON.stringify(output, null, 2), {
+		return new Response(SON.stringify(pretty), {
 			headers: {
 				'Content-Type': 'application/json;charset=utf-8',
 			  },
