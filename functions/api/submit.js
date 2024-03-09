b70bb0acca32e7cd03966ed49373b48b51460056
socialitin <lmacias@tournet.com>
2024-03-09 14:41:36 -0500

    Update submit.js

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -2,10 +2,12 @@
  * POST /api/submit
  */
 export async function onRequestPost(context) {
+    
     try {
       let input = await context.request.formData();
       console.log('pjson', input);
       let pretty = JSON.stringify([...input], null, 2);
+      
       return new Response(pretty, {
         headers: {
           'Content-Type': 'application/json;charset=utf-8',
@@ -15,3 +17,15 @@ export async function onRequestPost(context) {
       return new Response('Error parsing JSON content', { status: 400 });
     }
   }
+
+  export async function onRequest(context) {
+    const stmt = context.env.DB.prepare("UPDATE hosts SET CompanyName = 'Cali' WHERE CompanyName LIKE '%tn%' ");
+    const result = await stmt.run(); // Execute the prepared statement
+
+    // Check the result of the update operation
+    if (result.changes > 0) {
+        return new Response.json({ success: true, message: 'Records updated successfully.' });
+    } else {
+        return new Response.json({ success: false, message: 'No records were updated.' });
+    }
+}
