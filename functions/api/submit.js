45ec166fb117baea8006eb359de0b36976bc745b
socialitin <lmacias@tournet.com>
2024-03-14 16:47:28 -0400

    Update submit.js

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -16,7 +16,6 @@ export async function onRequestPost(context) {
 
         // Convert the JSON object to a string
         let jsonData = JSON.stringify(formDataObject);
-        sessionStorage.setItem("j2add",jsonData);      
 console.log('jdata is', jsonData);
         // Insert the JSON data into the SQLite database
        // await db.run("INSERT INTO hosts (pitching) VALUES (?)", [jsonData]);
