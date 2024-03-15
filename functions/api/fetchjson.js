a6671e701f2b604095cfab948f45676c7ec869cd
socialitin <lmacias@tournet.com>
2024-03-15 15:34:32 -0400

    Update fetchjson.js

--- functions/api/fetchjson.js
+++ functions/api/fetchjson.js
@@ -1,5 +1,5 @@
 export async function onRequest(context) {
-  const obj = await context.env.filterjson.put('NYCS.json');
+  const obj = await context.env.filterjson.get('NYCS.json');
   if (obj === null) {
     return new Response('Not found', { status: 404 });
   };
