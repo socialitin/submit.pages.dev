c950a5a93b2134ae7f9ffb8f6ced0d10ef7c3fd8
socialitin <lmacias@tournet.com>
2024-03-15 19:17:35 -0400

    b

--- functions/api/fetchputjson.js
+++ functions/api/fetchputjson.js
@@ -1,7 +1,20 @@
 export async function onRequest(context) {
-  const obj = await context.env.filterjson.put(j2publish);
-  if (obj === null) {
-    return new Response('Not found', { status: 404 });
-  };
-      return new Response(obj.body);
+
+    addEventListener('fetch', event => {
+        event.respondWith(handleRequest(event.request))
+        })
+        
+        async function handleRequest(request) {
+        const url = new URL(request.url)
+        const params = url.searchParams
+        const myParam = params.get('myParam')
+        
+        return new Response(`My parameter value is: ${myParam}`)
+        }
+
+ // const obj = await context.env.filterjson.put(j2publish);
+ // if (obj === null) {
+//    return new Response('Not found', { status: 404 });
+ // };
+   //   return new Response(obj.body);
 };
