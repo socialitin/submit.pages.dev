5d31acd9cadd257e6fe974740066cf7a64fbbcd4
socialitin <lmacias@tournet.com>
2024-03-16 11:32:41 -0400

    d

--- functions/api/fetchjson.js
+++ functions/api/fetchjson.js
@@ -1,9 +1,31 @@
 export async function onRequest(context) {
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request));
+});
+
+async function handleRequest(request) {
+  // Accessing query parameters from the request URL
+  const url = new URL(request.url);
+  const params = url.searchParams;
+  const paramValue = params.get('paramN');
+
+  // Your logic using the query parameters
+  // For example:
+  if (paramValue) {
+    
+      const obj = await context.env.filterjson.get('NYCS.json');
+      if (obj === null) {
+        return new Response('Not found', { status: 404 });
+      };
+          return new Response(obj.body);
   
-  const obj = await context.env.filterjson.get('NYCS.json');
-  if (obj === null) {
-    return new Response('Not found', { status: 404 });
-  };
-      return new Response(obj.body);
-};
+    //return new Response(`Value of paramName: ${paramValue}`);
+  } else {
+    return new Response('Parameter paramName is missing');
+  }
+}
+}
+
+
+
 
