5f11b97bcc82782f076b6deac4c7d3fda18323e0
socialitin <lmacias@tournet.com>
2024-03-16 11:41:02 -0400

    Update fetchjson.js

--- functions/api/fetchjson.js
+++ functions/api/fetchjson.js
@@ -7,7 +7,7 @@ async function handleRequest(request) {
   // Accessing query parameters from the request URL
   const url = new URL(request.url);
   const params = url.searchParams;
-  const paramValue = params.get('paramn');
+  const paramValue = params.get(paramn);
 
   // Your logic using the query parameters
   // For example:
