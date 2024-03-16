a5853be1eab281217d3cf50af57643335e5ed869
socialitin <lmacias@tournet.com>
2024-03-16 11:39:21 -0400

    d

--- functions/api/fetchjson.js
+++ functions/api/fetchjson.js
@@ -7,7 +7,7 @@ async function handleRequest(request) {
   // Accessing query parameters from the request URL
   const url = new URL(request.url);
   const params = url.searchParams;
-  const paramValue = params.get('paramN');
+  const paramValue = params.get('paramn');
 
   // Your logic using the query parameters
   // For example:
@@ -21,7 +21,7 @@ async function handleRequest(request) {
   
     //return new Response(`Value of paramName: ${paramValue}`);
   } else {
-    return new Response('Parameter paramName is missing');
+    return new Response(params);
   }
 }
 }
