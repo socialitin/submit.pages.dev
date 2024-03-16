f2259c25a1ac19b39991f1b288529ce0039c983d
socialitin <lmacias@tournet.com>
2024-03-16 12:14:18 -0400

    Update fetchjson.js

--- functions/api/fetchjson.js
+++ functions/api/fetchjson.js
@@ -1,25 +1,24 @@
-//export async function onRequest(context) {
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request));
-});
-
-async function handleRequest(request) {
+export async function onRequest(request) {
   // Accessing query parameters from the request URL
   const url = new URL(request.url);
   const params = url.searchParams;
-  //const paramValue = params.get('paramn');
-  const paramValue = URLSearchParams.get('paramn')
+  const paramValue = params.get('paramn');
+
   // Your logic using the query parameters
   // For example:
   if (paramValue) {
+    // Fetch JSON data
+    const response = await fetch('/api/fetchjson?paramn=' + paramValue);
+    if (!response.ok) {
+      return new Response('Failed to fetch JSON data', { status: 500 });
+    }
     
-    return new Response(paramValue);
+    const obj = await response.json();
+    return new Response(JSON.stringify(obj));
   } else {
- 
-    return new Response(params);
+    return new Response('Parameter "paramn" is missing', { status: 400 });
   }
 }
-
 
 
 
