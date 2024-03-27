a426d5c61bdffdd302393b441165566fcad60ccc
socialitin <lmacias@tournet.com>
2024-03-27 13:57:08 -0400

    Update fetchjson.js

--- functions/api/fetchjson.js
+++ functions/api/fetchjson.js
@@ -4,12 +4,6 @@ export async function onRequest(context) {
   if (obj === null) {
     return new Response('Not found', { status: 404 });
   };
-      //return new Response(obj.body);
-const j2upd = new Response(obj.body);
-      const ps = context.env.DB.prepare('SELECT * from hosts');
-      const data = await ps.first();
-        
-        const k2upd = Response.json(data);
-        return j2upd+k2upd;
+      return new Response(obj.body);
 };
 
