e5a5bfd38a877ce06bf84fa5bd5dda44777b00b8
socialitin <lmacias@tournet.com>
2024-03-27 16:09:03 -0400

    j

--- functions/api/d1tnupdate.js
+++ functions/api/d1tnupdate.js
@@ -1,20 +1,20 @@
 
     export async function onRequest(context) {
-        const stmt = context.env.DB.prepare("UPDATE hosts SET CompanyName = 'Pereira' WHERE CompanyName LIKE '%tn%' ");
-        const result = await stmt.run(); // Execute the prepared statement
+       //// const stmt = context.env.DB.prepare("UPDATE hosts SET CompanyName = 'Pereira' WHERE CompanyName LIKE '%tn%' ");
+       /// const result = await stmt.run(); // Execute the prepared statement
     
         // Check the result of the update operation
         if (result.changes > 0) {
-            return new Response.json({ success: true, message: 'Records updated successfully.' });
+           ///return new Response.json({ success: true, message: 'Records updated successfully.' });
         } else {
-            return new Response.json({ success: false, message: 'No records were updated.' });
+          ///  return new Response.json({ success: false, message: 'No records were updated.' });
         }
     
 
     /////new update
    /// export async function onRequest(context) {
-        const ps = context.env.DB.prepare('SELECT * from hosts');
-        const data = await ps.first();
+       const ps = context.env.DB.prepare('SELECT * from hosts');
+       const data = await ps.first();
           return Response.json(data);
      //const info = await env.DB.prepare(`INSERT INTO jsondata (pitch) VALUES ('{"City": "Cali"}')`);
      //const info = await env.DB.prepare(`UPDATE jsondata SET pitch = json_replace(pitch, '$.City', 'NYC') `);
@@ -22,12 +22,12 @@
     //return Response.json(dat);
     
     ///call tournet.worker to fetch json
-    const j2upd = context.env.grabjson.fetch('NYCS2.json');
+    const j2upd = context.env.filterjson.fetch('NYCS2.json');
     
     //const obj = await context.env.filterjson.get('NYCS2.json');
     if (j2upd === null) {
-      return new Response('Not found', { status: 404 });
+      ///return new Response('Not found', { status: 404 });
     };
-        return new Response(obj.body);
+       //// return new Response(obj.body);
       }
     
