ed4cc78745a81fba3aea066ce4d5cfeac3b26830
socialitin <lmacias@tournet.com>
2024-03-27 20:44:50 -0400

    Update d1tnupdate.js

--- functions/api/d1tnupdate.js
+++ functions/api/d1tnupdate.js
@@ -3,7 +3,7 @@
 
              const ps = context.env.DB.prepare('SELECT * from hosts');
        const data = await ps.first();
-          const data1 = Response.json(data);
+          const data1 = data;
           const p2p =JSON.parse(data1.pitching);
             const workerURL = 'https://tournet.socialitin.workers.dev/'; // Replace with your Worker's URL
             const response = await fetch(workerURL, {
@@ -17,7 +17,7 @@
             const data = await response.json();
             const j2upd = new Response(JSON.stringify(data));
             var newjson = j2upd["NYCS"].push(p2p);
-            return data1;
+            return p2p;
             //new Response(JSON.stringify(data), {
             //headers: { 'Content-Type': 'application/json' },
             //});
