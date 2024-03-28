aece8d1b8dcf091f41c161e0be6a8e1a58460613
socialitin <lmacias@tournet.com>
2024-03-27 21:38:54 -0400

    Update d1tnupdate.js

--- functions/api/d1tnupdate.js
+++ functions/api/d1tnupdate.js
@@ -16,8 +16,7 @@
             
             if (response.ok) {
             const data = await response.json();
-            const j2upd = new Response(data);
-            //new Response(JSON.stringify(data));
+            const j2upd = new Response(JSON.stringify(data));
             //var newjson = j2upd["NYCS"].push(p2p);
             return j2upd;
             //new Response(JSON.stringify(data), {
