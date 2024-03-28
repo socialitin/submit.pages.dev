1dcc1e165a86df3576a7bf9a02e9c6208032e971
socialitin <lmacias@tournet.com>
2024-03-28 13:37:33 -0400

    Update submit.js

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -31,7 +31,7 @@ console.log('jdata is', jsonData);
      const data = await response2.json();
      const j2upd = new Response(JSON.stringify(data));
      var newjson = j2upd["NYCS"].push(jsonData);
-    // return j2upd;
+     return j2upd;
      //new Response(JSON.stringify(data), {
      //headers: { 'Content-Type': 'application/json' },
      //});
@@ -46,18 +46,7 @@ console.log('jdata is', jsonData);
        // await db.run("INSERT INTO hosts (pitching) VALUES (?)", [jsonData]);
        //const stmt = context.env.DB.prepare("INSERT INTO hosts (pitching) VALUES (?),[jsonDate]");
 
-       const stmt = context.env.DB.prepare("UPDATE hosts SET pitching = ? WHERE CompanyName LIKE '%Pereirawas%' ");
-const response = await stmt.bind(jsonData).run(); 
 
-return new Response(jsonData, {
-            status: 200,
-            headers: {
-                'Content-Type': 'text/plain'
-            }
-        });
-    } catch (err) {
-        return new Response('Error inserting data into SQLite database', { status: 500 });
-    }
     
 }
   
