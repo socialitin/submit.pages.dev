fbf077264178626b4b22a09d0e337ab881f0a4ac
socialitin <lmacias@tournet.com>
2024-03-28 14:32:48 -0400

    Update submit.js

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -10,21 +10,6 @@ export async function onRequestPost(context) {
             formDataObject[name] = value;
         }
 
-        // Read the existing JSON file
-        ///const obj = await context.env.filterjson.get('NYCS.json');
-        //const filePath = 'path_to_your_json_file.json';
-        //const existingData = await fs.readFile(filePath, 'utf8');
-        ///const existingJson = JSON.parse(obj);
-    
-        // Update the JSON object with the new data
-        //existingJson.NYCS = formDataObject;
-
-        // Convert the updated JSON object to a string
-        //let updatedJsonData = JSON.stringify(existingJson);
-
-        // Write the updated JSON data back to the file
-       // await fs.writeFile(filePath, updatedJsonData, 'utf8');
-
         return formDataObject;
     } catch (err) {
         return new Response('Error updating JSON file', { status: 500 });
