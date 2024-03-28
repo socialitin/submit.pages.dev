0b52e915c035b6da4f7f93073cd1b72591edc0bb
socialitin <lmacias@tournet.com>
2024-03-27 22:45:25 -0400

    Update d1tnupdate.js

--- functions/api/d1tnupdate.js
+++ functions/api/d1tnupdate.js
@@ -6,8 +6,8 @@
        const data1 = await ps.first();
        //const data1 = await ps;
        //return Response.json(data);
-          const data2 = Response.json(data1);
-          const p2p = data2.pitching;
+          const p2p = Response.json(data1);
+          //const p2p = data2.pitching;
             const workerURL = 'https://tournet.socialitin.workers.dev/'; // Replace with your Worker's URL
             const response = await fetch(workerURL, {
             method: 'GET', // or 'POST', 'PUT', etc. depending on your Worker
@@ -19,8 +19,8 @@
             if (response.ok) {
             const data = await response.json();
             const j2upd = new Response(JSON.stringify(data));
-            //var newjson = j2upd["NYCS"].push(p2p);
-            return data2;
+            var newjson = j2upd["NYCS"].push(p2p);
+            return j2upd;
             //new Response(JSON.stringify(data), {
             //headers: { 'Content-Type': 'application/json' },
             //});
