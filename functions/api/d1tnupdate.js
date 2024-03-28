26bfcc267b90ebe6ff49ff89f301007f26f338d4
socialitin <lmacias@tournet.com>
2024-03-27 22:18:09 -0400

    Update d1tnupdate.js

--- functions/api/d1tnupdate.js
+++ functions/api/d1tnupdate.js
@@ -2,7 +2,7 @@
     export async function onRequest(context) {
 
            //  const ps = context.env.DB.prepare('SELECT * from hosts');
-const ps = context.env.DB.prepare('SELECT json_extract(pitching, "$.Pitching") AS extracted_pitch FROM hosts WHERE CustomerId = 111');             
+const ps = context.env.DB.prepare('SELECT json_extract(pitching, "$.pitching") AS extracted_pitch FROM hosts WHERE CustomerId = 111');             
        const data1 = await ps.first();
        //return Response.json(data);
           const data2 = Response.json(data1);
