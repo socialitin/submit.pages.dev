59f2c7a3e3f92f76930c040be49aaa6d686701f3
socialitin <lmacias@tournet.com>
2024-03-29 13:08:28 -0400

    Update submit.js

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -17,56 +17,13 @@ export async function onRequestPost({ request }) {
         }
         console.log(output);
 
-        let existingData = await getExistingData();
-        if (!existingData) {
-            existingData = {};
-        }
-        // Merge existing data with new data
-        Object.assign(existingData, output);
-
-        let json = JSON.stringify(existingData, null, 2);
-        console.log(json);
-
-        // Write JSON data to file
-        await writeToJSONFile(json);
-
-        return new Response(json, {
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8',
-            },
-        });
+  return output;
 
     } catch (err) {
        return new Response('Error parsing JSON content', output);
     }
 }
 
-// Function to fetch existing JSON data
-async function getExistingData() {
-    try {
-        const response = await R2.get("https://pub-ff67a151dd104cf6b171f45a47c36526.r2.dev/NYCS2.json");
-        if (!response.ok) {
-            return null;
-        }
-        const jsonData = await response.json();
-        return jsonData;
-    } catch (error) {
-        return null;
-    }
-}
 
-// Function to write JSON data to file
-async function writeToJSONFile(jsonData) {
-    const response = await R2.put("https://pub-ff67a151dd104cf6b171f45a47c36526.r2.dev/NYCS2.json", {
-        body: jsonData,
-        headers: {
-            "Content-Type": "application/json;charset=utf-8"
-        }
-    });
-
-    if (!response.ok) {
-        throw new Error("Failed to write JSON data to file");
-    }
-}
 
   
