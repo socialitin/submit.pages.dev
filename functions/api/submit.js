ee0cb68bbcb5103e14714bcd71422a27fced4291
socialitin <lmacias@tournet.com>
2024-03-29 13:18:06 -0400

    Update submit.js

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -1,25 +1,37 @@
-// POST /api/submit
-
-export async function onRequestPost({ request }) {
+/**
+ * POST /api/submit
+ */
+export async function onRequestPost(context) {
     try {
-        let input = await request.formData();
-        console.log(input);
-        // Convert FormData to JSON
-        // NOTE: Allows multiple values per key
-        let tmp, output = {};
-        for (let [key, value] of input) {
-            tmp = output[key];
-            if (tmp === undefined) {
-                output[key] = value;
-            } else {
-                output[key] = [].concat(tmp, value);
-            }
+      //  const db = context.env.DB;
+
+        // Parse form data from the request
+        let formData = await context.request.formData();
+
+        // Convert form data to JSON object
+        let formDataObject = {};
+        for (const [name, value] of formData.entries()) {
+            formDataObject[name] = value;
         }
-        console.log(output);
 
-  return output;
+        // Convert the JSON object to a string
+        let jsonData = JSON.stringify(formDataObject);
+console.log('jdata is', jsonData);
+        // Insert the JSON data into the SQLite database
+       // await db.run("INSERT INTO hosts (pitching) VALUES (?)", [jsonData]);
+       //const stmt = context.env.DB.prepare("INSERT INTO hosts (pitching) VALUES (?),[jsonDate]");
 
-    } finally{}
+      
+        const obj = await context.env.filterjson.get('NYCS.json');
+        if (obj === null) {
+          return new Response('Not found', { status: 404 });
+        };
+            return new Response(obj.body);
+      
+
+    } catch (err) {
+        return new Response('Error inserting data into SQLite database', { status: 500 });
+    }
 }
 
 
