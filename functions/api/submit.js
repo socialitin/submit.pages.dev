331d03f2e4854df8ef017a45c8880ae806c31c0f
socialitin <lmacias@tournet.com>
2024-03-29 14:01:25 -0400

    Update submit.js

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -22,16 +22,27 @@ console.log('jdata is', jsonData);
        //const stmt = context.env.DB.prepare("INSERT INTO hosts (pitching) VALUES (?),[jsonDate]");
 
       
-        const obj = await context.env.filterjson.get('NYCS.json');
+        const obj = await context.env.filterjson.get('NYCS2.json');
         if (obj === null) {
           return new Response('Not found', { status: 404 });
         };
 
-        const mixj = Object.assign(obj, jsonData);
+        //const mixj = Object.assign(obj, jsonData);
+        // Merge existing data with new data
+        for (const [key, value] of Object.entries(jsonData)) {
+            if (Array.isArray(obj[key])) {
+                obj[key].push(...value);
+            } else {
+                obj[key] = value;
+            }
+        }
+
+        let jMrgd = JSON.stringify(obj, null, 2);
+        console.log(jMrgd);
 
         //let json = JSON.stringify(existingData, null, 2);
 
-            return new Response(jsonData);
+            return new Response(jMrgd);
       
 
     } catch (err) {
