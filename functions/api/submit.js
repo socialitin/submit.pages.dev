66cab72536e0ab797feed054f89c5e259c100ed3
socialitin <lmacias@tournet.com>
2024-03-30 12:29:16 -0400

    Update submit.js

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -27,8 +27,9 @@ console.log('jdata is', jsonData);
         if (obj === null) {
           return new Response('Not found', { status: 404 });
         };
-
-        const jMrgd = Object.assign(obj, jsonData);
+        obj["440-2"] = jsonData;
+        const jMrgd = obj;
+        //Object.assign(obj, jsonData);
         // Check if grouping key exists in both existing data and new data, and if their content matches
         //const groupingKey = '440-2'; // Replace 'groupingKey' with your actual grouping key
       
