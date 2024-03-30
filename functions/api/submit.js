0f674198a404845e8a9393f4fc78eeac0b2b9faa
socialitin <lmacias@tournet.com>
2024-03-30 12:38:51 -0400

    Update submit.js

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -23,7 +23,7 @@ console.log('jdata is', jsonData);
        //const stmt = context.env.DB.prepare("INSERT INTO hosts (pitching) VALUES (?),[jsonDate]");
 
       
-        const obj = await context.env.filterjson.get('NYCS3.json');
+        const obj = await context.env.filterjson.get('NYCS2.json');
         if (obj === null) {
           return new Response('Not found', { status: 404 });
         };
