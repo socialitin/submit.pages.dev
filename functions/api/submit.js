04db80b22a039342691fc897856d7b6a972a0778
socialitin <lmacias@tournet.com>
2024-04-02 17:15:49 -0400

    Update submit.js

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -30,7 +30,7 @@ console.log('jdata is', jsonData);
 
         // obj.body['440-2'] = jsonData;  
 //const data1 =JSON.parse(data);
-        data['440-2'].City = 'Cali';
+ //       data['440-2'].City = 'Cali';
         const origData = data;
         // Iterate over each object in the array
         const subgroupID = "440-2"; // The ID of the subgroup you want to modify
