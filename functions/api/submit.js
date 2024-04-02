323b0a442b9d6e9f66e52aae8a281a73ace2408f
socialitin <lmacias@tournet.com>
2024-04-02 14:59:26 -0400

    Update submit.js

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -80,7 +80,7 @@ if (!found) {
         }
   
         // Return a success response
-        return new Response(JSON.stringify(jsonData));
+        return new Response(jsonData);
           ///('JSON data updated successfully', { status: 200 });
       } catch (error) {
         console.error('Error:', error);
