ee585db4a15798ebc38e7ab48959aa9a5c2c5f77
socialitin <lmacias@tournet.com>
2024-04-03 13:59:51 -0400

    Update submit.js

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -20,7 +20,7 @@ export async function onRequestPost(context) {
         //let jsonData = jsonData1.delete_prefix('"').delete_suffix('"')
         //jsonData1.slice(1,0);
         //let jsonData = jsonData2.slice(0,-1);
-        let jsonData = jsonData1.substring(1,length.jsonData-1);
+        let jsonData = jsonData1.substring(1,jsonData.length-1);
 console.log('jdata is',jsonData);
         // Insert the JSON data into the SQLite database
        // await db.run("INSERT INTO hosts (pitching) VALUES (?)", [jsonData]);
