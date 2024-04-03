2e485b60cd9cd81c59df156ab93f23c596b53ee2
socialitin <lmacias@tournet.com>
2024-04-03 13:12:59 -0400

    Update submit.js

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -16,8 +16,8 @@ export async function onRequestPost(context) {
 
         // Convert the JSON object to a string
         let jsonData1 = JSON.stringify(formDataObject);
-        //let jsonData = jsonData1.replace(/^"|"$/g, "");
-        let jsonData = jsonData1.delete_prefix('"').delete_suffix('"')
+        let jsonData = jsonData1.replace(/^"|"$/g, '');
+        //let jsonData = jsonData1.delete_prefix('"').delete_suffix('"')
         //jsonData1.slice(1);
         //let jsonData = jsonData2.slice(0,-1);
 console.log('jdata is',jsonData);
