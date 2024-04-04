4372c064d42046e1cbcbe8873446027df091caee
socialitin <lmacias@tournet.com>
2024-04-04 13:05:49 -0400

    Update submit.js

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -4,8 +4,28 @@
  */
 export async function onRequestPost(context) {
   try {
-      const formData = await context.request.formData();
-      const formDataObject = Object.fromEntries(formData.entries());
+
+    let formData = await context.request.formData();
+
+    // Convert form data to JSON object
+    let formDataObject = {};
+    for (const [name, value] of formData.entries()) {
+        formDataObject[name] = value;
+    }
+
+    // Convert the JSON object to a string
+    let jsonData1 = JSON.stringify(formDataObject);
+    //let jsonData = jsonData1.replace(/^"|"$/g, '');
+   
+    //jsonData1.slice(1,0);
+    //let jsonData = jsonData2.slice(0,-1);
+    //let jsonData = jsonData1.substring(1,jsonData1.length-1);
+    let jsonData2 = jsonData1.replace('"{','{');
+    let jsonData = jsonData2.replace('}"','}');
+console.log('jdata is',jsonData);
+
+      ///const formData = await context.request.formData();
+      ///const formDataObject = Object.fromEntries(formData.entries());
       
       // fetch to retrieve existing data 
       const j2upd = await context.env.filterjson.get('NYCS3.json');
@@ -18,7 +38,8 @@ export async function onRequestPost(context) {
 //const data1 =JSON.parse(data);
 //       data['440-2'].City = 'Cali';
       //const origData1 = JSON.parse(data);
-      const origData = j2upData['data'];
+      const origData = jsonData;
+      ///j2upData['data'];
 
       //const origData = { data: [{ "440-2": {} }] }; // This should be replaced with your actual fetch logic
 
@@ -59,7 +80,6 @@ export async function onRequestPost(context) {
       return new Response('JSON data updated successfully', { status: 200 });
   } catch (err) {
       console.error('Error processing request:', err);
-      return (JSON.stringify(origData));
-      //new Response('Error processing request', { status: 500 });
+      return new Response('Error processing request', { status: 500 });
   }
 }
