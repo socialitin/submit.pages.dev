9341c30193b8af71e91da46c483bf2ea9aac8ba8
socialitin <lmacias@tournet.com>
2024-04-14 17:23:33 -0400

    Update subscribe.js

--- functions/api/subscribe.js
+++ functions/api/subscribe.js
@@ -20,7 +20,7 @@ export async function onRequestPost(context) {
         const fileName = `${dateString}.json`;
 
         // Assuming the base URL for your Cloudflare Worker that handles file storage
-        const storageUrl = 'https://tournet.socialitin.workers.dev/';
+        const storageUrl = 'https://tournet.socialitin.workers.dev/test.json';
         //`https://your-cloudflare-worker-url.com/test1.json`;
 
         // Prepare the PUT request to store the JSON data
