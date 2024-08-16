e23ca3164894bd172261475868db3d8e231ffc7f
socialitin <lmacias@tournet.com>
2024-08-15 20:30:26 -0400

    h
    j

---
+++ functions/api/pub-sub-publish.js
@@ -0,0 +1,25 @@
+fetch('https://https://pub-sub.socialitin.workers.dev/publish', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ message: 'Hello, subscribers!' }),
+  });
+
+  function publish(message) {
+    fetch('https://https://pub-sub.socialitin.workers.dev/publish', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ message: message }),
+    })
+    .then(response => response.text())
+    .then(data => {
+        console.log('Message sent:', data);
+    })
+    .catch(error => {
+        console.error('Error sending message:', error);
+    });
+}
+ 
