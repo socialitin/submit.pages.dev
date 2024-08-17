a837cd1122264af17981509823c7cb1676bdc814
socialitin <lmacias@tournet.com>
2024-08-17 13:02:16 -0400

    r

---
+++ pub-sub/pub-sub/src/pub-sub-subscribe.js
@@ -0,0 +1,20 @@
+function subscribe() {
+  const ws = new WebSocket('wss://https://pub-sub.socialitin.workers.dev/subscribe');
+  ws.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      if (window.onMessageReceived) {
+          window.onMessageReceived(data);
+      } else {
+          console.log('Received:', data.message);
+      }
+  };
+  ws.onopen = () => {
+      console.log('Subscribed to the channel');
+  };
+  ws.onclose = () => {
+      console.log('Subscription closed');
+  };
+  ws.onerror = (error) => {
+      console.error('WebSocket error:', error);
+  };
+}
