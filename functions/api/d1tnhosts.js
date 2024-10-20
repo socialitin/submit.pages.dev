0eae5503ba84c51b4c2547938ed5723852e7cab2
socialitin <lmacias@tournet.com>
2024-10-19 21:38:05 -0400

    Update d1tnhosts.js

--- functions/api/d1tnhosts.js
+++ functions/api/d1tnhosts.js
@@ -1,12 +1,16 @@
   export async function onRequest(context) {
-
-    const cust_Id = '440';  
-    const ps = context.env.DB.prepare('SELECT * FROM streams WHERE CustomerId = ?');  
-    const data = await ps.all(cust_Id);  // Use all() directly on the prepared statement with parameters  
-    return Response.json(data);
+   
+    const ps = context.env.DB.prepare('SELECT * from streams where CustomerId = "44o" ');
+    const data = await ps.all();
+    
+      return Response.json(data);
  //const info = await env.DB.prepare(`INSERT INTO jsondata (pitch) VALUES ('{"City": "Cali"}')`);
  //const info = await env.DB.prepare(`UPDATE jsondata SET pitch = json_replace(pitch, '$.City', 'NYC') `);
 //const dat = await info;
 //return Response.json(dat);
+//const cust_Id = '440';  
+//const ps = context.env.DB.prepare('SELECT * FROM streams WHERE CustomerId = ?');  
+//const data = await ps.all(cust_Id);  // Use all() directly on the prepared statement with parameters  
+//return Response.json(data);////
 
   }
