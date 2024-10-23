86f7e32854c36613f78327b072ac741052c7f3a8
socialitin <lmacias@tournet.com>
2024-10-23 18:10:29 -0400

    Update d1tnhosts.js

--- functions/api/d1tnhosts.js
+++ functions/api/d1tnhosts.js
@@ -4,5 +4,5 @@ export async function onRequest(context) {
 //return new Response(JSON.stringify(data))
  const ps = context.env.DB.prepare('SELECT ROWID, CompanyName,ContactName,CountryIataRegion, CustomerId,DateTime,Status,pitching from streams where CustomerId = "440" ');
  const data = await ps.all();
-    return Response.data.json();
+ return Response.json(data);
 }
