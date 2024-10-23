d3b837579302f3b6cacbee2d5ea38830c0011b02
socialitin <lmacias@tournet.com>
2024-10-23 18:24:38 -0400

    Update d1tnhosts.js

--- functions/api/d1tnhosts.js
+++ functions/api/d1tnhosts.js
@@ -1,32 +1,32 @@
-export async function onRequest(context) {
-  // Extract the CustomerId from the query parameters
-  const url = new URL(request.url);
-  const customerId = url.searchParams.get('CustomerId'); 
- // Prepare the SQL statement with a parameter placeholder
- const ps = env.DB.prepare(`
-  SELECT 
-    ROWID, 
-    CompanyName,
-    ContactName,
-    CountryIataRegion, 
-    CustomerId,
-    DateTime,
-    Status,
-    pitching 
-  FROM streams 
-  WHERE CustomerId = ?
-`);
+  export async function onRequest(context) {
+    // Extract the CustomerId from the query parameters
+    const url = new URL(request.url);
+    const customerId = url.searchParams.get('CustomerId'); 
+   // Prepare the SQL statement with a parameter placeholder
+   const ps = context.env.DB.prepare(`
+    SELECT 
+      ROWID, 
+      CompanyName,
+      ContactName,
+      CountryIataRegion, 
+      CustomerId,
+      DateTime,
+      Status,
+      pitching 
+    FROM streams 
+    WHERE CustomerId = ?
+  `);
 
-// Execute the prepared statement with the CustomerId as a parameter
-const data = await ps.all([customerId]);
+  // Execute the prepared statement with the CustomerId as a parameter
+  const data = await ps.all([customerId]);
 
- //const ps = context.env.DB.prepare('SELECT ROWID, CompanyName,ContactName,CountryIataRegion, CustomerId,DateTime,Status,pitching from streams where CustomerId = "440" ');
- // const data = await ps.all();
-  
-    return Response.json(data);
+   //const ps = context.env.DB.prepare('SELECT ROWID, CompanyName,ContactName,CountryIataRegion, CustomerId,DateTime,Status,pitching from streams where CustomerId = "440" ');
+   // const data = await ps.all();
+    
+      return Response.json(data);
+ 
 
-
-}
+  }
 
  //const ps = context.env.DB.prepare('SELECT ROWID, CompanyName,ContactName,CountryIataRegion, CustomerId,DateTime,Status,pitching from streams where CustomerId = "440" ');
  //const data = await ps.all();
