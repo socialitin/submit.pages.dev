e05523a0872289ae8fe49eb6dafc58cb49ca20a7
socialitin <lmacias@tournet.com>
2024-10-23 14:59:55 -0400

    Update d1tnhosts.js

--- functions/api/d1tnhosts.js
+++ functions/api/d1tnhosts.js
@@ -1,8 +1,9 @@
   export async function onRequest(context) {
    
-    const ps = context.env.DB.prepare('SELECT ROWID, CompanyName,ContactName,CountryIataRegion, CustomerId,DateTime,Status,pitching from streams where CustomerId = "440" ');
-    const data = await ps.all();
-    
+   // const ps = context.env.DB.prepare('SELECT ROWID, CompanyName,ContactName,CountryIataRegion, CustomerId,DateTime,Status,pitching from streams where CustomerId = "440" ');
+   // const data = await ps.all();
+   const ps = context.env.DB.prepare('SELECT * FROM streams WHERE CustomerId = ?');  
+   const data = await ps.all([cust_Id]);
       return Response.json(data);
  //const info = await env.DB.prepare(`INSERT INTO jsondata (pitch) VALUES ('{"City": "Cali"}')`);
  //const info = await env.DB.prepare(`UPDATE jsondata SET pitch = json_replace(pitch, '$.City', 'NYC') `);
