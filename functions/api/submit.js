377978f910d97d57fd9c6a2d2a75b44b71494324
socialitin <lmacias@tournet.com>
2024-10-26 18:26:08 -0400

    Update submit.js

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -14,10 +14,13 @@ export async function onRequestPost(context) {
             formDataObject[name] = value;
         }
         // Convert the JSON object to a string
-        let pWrap = formDataObject;
-        var pWrapa = pWrap + '{ "'+document.getElementById('jdata').cpit+'":'; 
-        var jsonData = JSON.stringify(pWrapa + '}');
+        let jsonData = JSON.stringify(formDataObject);
 console.log('jdata is', jsonData);
+//let pWrapb = {};
+//let pWrap = formDataObject;
+//var pWrapa = pWrap + '{ "'+document.getElementById('jdata').cpit+'":'; 
+//var jsonData = JSON.stringify(pWrapa + '}');
+//console.log('jdata is', jsonData);
         // Insert the JSON data into the SQLite database
        // await db.run("INSERT INTO hosts (pitching) VALUES (?)", [jsonData]);
        //const stmt = context.env.DB.prepare("INSERT INTO hosts (pitching) VALUES (?),[jsonDate]");
