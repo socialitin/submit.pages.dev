8151b5b0eac18ccc0be4c12cfb3f85abb9b69bed
socialitin <lmacias@tournet.com>
2025-09-04 18:32:19 -0400

    Update session.js

--- functions/api/session/session.js
+++ functions/api/session/session.js
@@ -11,6 +11,13 @@ export default {
         });
       }
 
+      if (!env.MY_BUCKET) {
+        return new Response('R2 binding missing', {
+          status: 500,
+          headers: { 'Access-Control-Allow-Origin': '*' }
+        });
+      }
+
       if (request.method === 'GET') {
         const object = await env.MY_BUCKET.get(fileName);
         if (!object) {
@@ -51,53 +58,4 @@ export default {
     }
   },
 };
-//       const { keys } = await request.json();
-//       if (!Array.isArray(keys) || keys.some(k => typeof k !== 'string')) {
-//         return new Response('Invalid keys', {
-//           status: 400,
-//           headers: { 'Access-Control-Allow-Origin': '*' }
-//         });
-//       }
-//       // Fetch all sessions
-//       const sessions = await Promise.all(keys.map(key => env.MY_BUCKET.get(key)));
-//       const results = {};
-//       sessions.forEach((session, index) => {
-//         if (session) {
-//           results[keys[index]] = session.text();
-//         }
-//       });
-//       return new Response(JSON.stringify(results), {
-//         headers: {
-//           'Content-Type': 'application/json',
-//           'Access-Control-Allow-Origin': '*'
-//         }
-//       });
-//     }
-//   }
-// };
-//       if (request.method === 'POST') {
-//         // Save or update a session
-//         const { key, data } = await request.json();
-//         if (typeof key !== 'string' || typeof data !== 'object') {
-//           return new Response('Invalid key or data', {
-//             status: 400,
-//             headers: { 'Access-Control-Allow-Origin': '*' }
-//           });
-//         }
-//         // Save or update the session
-//         await env.MY_BUCKET.put(key, JSON.stringify(data), {
-//           httpMetadata: { contentType: 'application/json' }
-//         });
-//         return new Response('Session saved/updated', {
-//           status: 200,
-//           headers: { 'Access-Control-Allow-Origin': '*' }
-//         });
-//       }
-//     } catch (error) {
-//       return new Response('Internal Error: ' + error.message, {
-//         status: 500,
-//         headers: { 'Access-Control-Allow-Origin': '*' }
-//       });
-//     }
-//   }
-// };
+
